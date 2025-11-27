afa9e81aea6ea5e60b44f5e56f17270594f1d324
Christopher-E <crristopher@gmail.com>
2025-11-27 20:15:12 +0000

    Set up Scalar as a self documenting API interface. Probably not as good as swagger but its supported in .net 9 and 10

--- Tally/Tally.Api/Program.cs
+++ Tally/Tally.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using Tally.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +18,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
