12fc38940bb722c979669af9836d371047c8bded
Christopher Thomas Edmunds <crristopher@gmail.com>
2025-12-11 18:59:05 +0000

    Update Tally.Web.csproj: bump WebAssembly.Server to 10.0.1
    Upgraded the Microsoft.AspNetCore.Components.WebAssembly.Server package reference from version 9.0.9 to 10.0.1 in Tally.Web.csproj to ensure compatibility with the latest features and improvements. No other dependencies were modified.

--- Tally/Tally.Web/Tally.Web/obj/Debug/net10.0/Tally.Web.AssemblyInfo.cs
+++ Tally/Tally.Web/Tally.Web/obj/Debug/net10.0/Tally.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tally.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0bf283e9627208ff668f04aed223c29f98e753")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc415961fc4a5cb437435c400383a946cf902611")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tally.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tally.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
